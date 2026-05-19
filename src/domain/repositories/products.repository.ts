9ee948d310ed8aa0acddda95212b6eb365e3e5dd
Gabriel-Te <gabrielteixeiracorrea5@gmail.com>
2026-05-18 21:02:56 -0300

    refactor: remove deprecated list method from ProductsRepository

--- src/domain/repositories/products.repository.ts
+++ src/domain/repositories/products.repository.ts
@@ -60,33 +60,7 @@ export class ProductsRepository {
     });
   }
 
-  async list({
-    artisanId,
-    categoryId,
-    id,
-    title,
-  }: ListProductsInput): Promise<Product[]> {
-    const productsWithPhotos = await this.prisma.product.findMany({
-      where: {
-        artisanId,
-        categoryIds: categoryId ? { has: BigInt(categoryId) } : undefined,
-        id,
-        title: title ? { contains: title, mode: 'insensitive' } : undefined,
-      },
-      orderBy: { createdAt: 'desc' },
-      include: {
-        images: true,
-      },
-    });
 
-    return productsWithPhotos.map((product) => ({
-      ...product,
-      photos: product.images.map((photo) => ({
-        attachmentId: photo.id,
-        productId: photo.productId!,
-      })),
-    }));
-  }
 
   async listWithAuthorsAndArtisans({
     artisanId,
