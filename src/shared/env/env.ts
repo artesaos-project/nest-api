8fe4a42860d8a5cf89a5d0be6e86e1450550f739
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-06-11 22:58:09 -0300

    fix: add front end url to env schema validation

--- src/shared/env/env.ts
+++ src/shared/env/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
+  FRONTEND_URL: z.string().url(),
   DATABASE_URL: z.string().url(),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
